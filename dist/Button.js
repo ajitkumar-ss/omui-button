d3c416553146017a0386cbf0acdf6c5578751970
ajitkumar-ss <ajitkumar100296@gmail.com>
2025-07-03 17:46:05 +0530

    build

--- dist/Button.js
+++ dist/Button.js
@@ -5,7 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
-require("./Button.css");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 var Button = function Button(_ref) {
   var label = _ref.label,
@@ -14,10 +13,21 @@ var Button = function Button(_ref) {
     type = _ref$type === void 0 ? 'button' : _ref$type,
     _ref$className = _ref.className,
     className = _ref$className === void 0 ? '' : _ref$className;
+  var baseStyle = {
+    padding: '0.5rem 1rem',
+    backgroundColor: '#2563eb',
+    color: '#ffffff',
+    border: 'none',
+    borderRadius: '0.5rem',
+    fontSize: '1rem',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease'
+  };
   return /*#__PURE__*/_react["default"].createElement("button", {
     type: type,
     onClick: onClick,
-    className: "my-btn ".concat(className)
+    style: baseStyle,
+    className: className
   }, label);
 };
 var _default = exports["default"] = Button;
